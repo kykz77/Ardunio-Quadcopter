03fa4b05df076fc6f55190cf4dc1452f0ba28abf
OpenSourceEdu2023 <87601841+kykz77@users.noreply.github.com>
2024-02-18 14:57:58 +1100

    Removed print statements slowing down code

--- Flight_Controller.ino
+++ Flight_Controller.ino
@@ -5,16 +5,16 @@
 //ESCs
 Servo esc1, esc2, esc3, esc4;
 float esc1_pulse,esc2_pulse,esc3_pulse,esc4_pulse;
-//Variables for reading gyroscope and accelerometer
+
+//IMU Variables
 float accX, accY, accZ, gyroX, gyroY, gyroZ;
 float accX_angle, accY_angle, accZ_angle, gyroX_angle, gyroY_angle, gyroZ_angle;
-float accX_angle_error = -0.12;
-float accY_angle_error = 5.41;
-float gyroX_error = -2.60;
-float gyroY_error = 0.26;
-float gyroZ_error = 1.76;
+float accX_angle_error = -3.52;
+float accY_angle_error = 7.33;
+float gyroX_error = -2.57;
+float gyroY_error = 0.19;
+float gyroZ_error = 1.57;
 float roll, pitch, yaw;
-float accX_error, accY_error, gyroX_error, gyroY_error, gyroZ_error;
 float time_ellp, time_curr, time_prev;
 
 //Kalman Filter
@@ -26,34 +26,39 @@ float K_pitch_angle_u = 4;
 float last_K_pitch = 0;
 float K_output[] = {0,0};
 
+//RC Transmitter and Reciever
 //Variables for reading Channels
 byte CH1_prev, CH2_prev, CH3_prev, CH4_prev;
 unsigned long ISR_timer1, ISR_timer2, ISR_timer3, ISR_timer4;
 volatile int CH1_input, CH2_input, CH3_input, CH4_input;
+//Variables for reciever Angles
+float recX_angle,recY_angle,recZ_angle;
 
 //Variables for PID controller
 //Roll X-axis
-float p_gain_roll = 0.0;
+float p_gain_roll = 1.3;
 float i_gain_roll = 0.0;
-float d_gain_roll = 0.0;
+float d_gain_roll = 15.0;
 float roll_output = 0.0;
 float roll_p = 0.0;
 float roll_i = 0.0;
 float roll_d = 0.0;
 float roll_curr_error = 0.0;
 float roll_last_error = 0.0;
-int max_roll = 200; 
+int max_roll = 300; 
+
 //Pitch Y-axis
 float p_gain_pitch = p_gain_roll;
 float i_gain_pitch = i_gain_roll;
 float d_gain_pitch = d_gain_roll;
-float pitch_p = 3.0;
-float pitch_i = 0.01;
+float pitch_p = 0.0;
+float pitch_i = 0.0;
 float pitch_d = 0.0;
 float pitch_output = 0;
 float pitch_curr_error = 0.0;
 float pitch_last_error = 0.0;
-int max_pitch = 200; 
+int max_pitch = 300;
+
 //Yaw Z-axis
 float p_gain_yaw = 0.0;
 float i_gain_yaw = 0.0;
@@ -64,41 +69,46 @@ float yaw_d = 0.0;
 float yaw_output = 0;
 float yaw_curr_error = 0.0;
 float yaw_last_error = 0.0;
-int max_yaw = 200; 
-//Reciever
-float recX_angle,recY_angle,recZ_angle;
-//Time Ellapsed
-float pid_time_prev;
-float pid_time_elp;
-float time = 0;
+int max_yaw = 300; 
 
 void setup() {
- //Activate PCI and Port B
- PCICR |= (1 << PCIE0); //Activating Port B for PCI
- PCMSK0 |= (1 << PCINT0); //pin 8, CH1
- PCMSK0 |= (1 << PCINT1); //pin 9, CH2
- PCMSK0 |= (1 << PCINT2); //pin 10, CH3
- PCMSK0 |= (1 << PCINT3); //pin 11, CH4
- Serial.begin(9600);
+  //Turning on Serial 
+  Serial.begin(115200);
+  delay(10);
+  Interrupt_Setup();
+  delay(10);
+  IMU_Setup();
+  delay(10);
+  ESC_Setup();
+  delay(2000);
+}
+
+void loop() {
+  read_IMU();
+  read_Signal();
+  PID_Control();
+  ESC_Pulse();
+}
+
+////Setup Functions////
+void IMU_Setup() {
  Wire.begin();
+ //Writing to MPU6050 slave address and activating it
  Wire.beginTransmission(0x68);
  Wire.write(0x6B);
  Wire.write(0x00);
  Wire.endTransmission(true);
- delay(10);
- //Setting up ESCs
- DDRD |= B11110000;
- ESC_setup();
- delay(10);
 }
 
-void loop() {
- readIMU();
- readSignal();
- PID_Control();
- ESC_pulse();
+void Interrupt_Setup() {
+ PCICR |= (1 << PCIE0); //Activating Port B for Pin Change Interrupts (PCI)
+ PCMSK0 |= (1 << PCINT0); //pin 8, CH1
+ PCMSK0 |= (1 << PCINT1); //pin 9, CH2
+ PCMSK0 |= (1 << PCINT2); //pin 10, CH3
+ PCMSK0 |= (1 << PCINT3); //pin 11, CH4
 }
 
+//Interrupt Sub Routine (ISR), will be called whenever PCI occurs
 ISR(PCINT0_vect) {
   if (CH1_prev == 0 && (PINB & B00000001)) {
     CH1_prev = 1;
@@ -130,33 +140,39 @@ ISR(PCINT0_vect) {
   }
 }
 
-void ESC_setup() {
- esc1.attach(4,850,1860);
- esc1.write(0);
- delay(2000);
- esc2.attach(5,850,1860);
- esc2.write(0);
- delay(2000);
- esc3.attach(6,850,1860);
- esc3.write(0);
- delay(2000);
- esc4.attach(7,850,1860);
- esc4.write(0);
- delay(2000);
-}
+void ESC_Setup() {
+  DDRD |= B11110000; //Setting the Port D pins (4,5,6,7) to output mode
+  //Pairing up pins with ESCs, then providing minimum throttle to arm ESCs
+  
+  esc1.writeMicroseconds(1000);
+  esc2.writeMicroseconds(1000);
+  esc3.writeMicroseconds(1000);
+  esc4.writeMicroseconds(1000);
+  esc1.attach(4, 1000, 2000);
+  esc2.attach(5, 1000, 2000);
+  esc3.attach(6, 1000, 2000);
+  esc4.attach(7, 1000, 2000);
+};
 
-void readIMU() {
-  //Reading accelerometer values
+////Loop Functions////
+
+//Reading IMU Functions//
+
+//Reading accelerometer values
+void read_Acc() {
  Wire.beginTransmission(0x68);
  Wire.write(0x3B);
  Wire.endTransmission(false);
  Wire.requestFrom(0x68,6,true);
- accX = (Wire.read() << 8 | Wire.read()) / 16384.0; //Combine the stuff since they're stored in 2 diff registers (both 8 bit)
+ accX = (Wire.read() << 8 | Wire.read()) / 16384.0; //Combine data since they are stored in 2 different registers (both 8 bit)
  accY = (Wire.read() << 8 | Wire.read()) / 16384.0;
  accZ = (Wire.read() << 8 | Wire.read()) / 16384.0;
  accX_angle = (atan(accY / sqrt(pow(accX,2) + pow(accZ,2))))*(180/PI) - accX_angle_error;
  accY_angle = (atan(-1 * accX / sqrt(pow(accY,2) + pow(accZ,2))))*(180/PI) - accY_angle_error;
- //Reading gyroscope values
+}
+
+//Reading gyroscope values
+void read_Gyro() {
  time_prev = time_curr;
  time_curr = millis();
  time_ellp =( time_curr - time_prev)/1000;
@@ -164,204 +180,12 @@ void readIMU() {
  Wire.write(0x43);
  Wire.endTransmission(false);
  Wire.requestFrom(0x68,6,true);
- gyroX = (Wire.read() << 8 | Wire.read()) / 131.0 - gyroX_error; //Combine the stuff since they're stored in 2 diff registers (both 8 bit)
+ gyroX = (Wire.read() << 8 | Wire.read()) / 131.0 - gyroX_error; //Combine data since they are stored in 2 different registers (both 8 bit)
  gyroY = (Wire.read() << 8 | Wire.read()) / 131.0 - gyroY_error;
- gyroZ = (Wire.read() << 8 | Wire.read()) / 131.0 - gyroZ_error; 
- run_filters();
- roll = K_roll_angle;
- pitch = K_pitch_angle;
- yaw = yaw + gyroZ * time_ellp;
- Serial.println("Angles:");
- Serial.print(roll);
- Serial.print("/");
- Serial.print(pitch);
- Serial.print("/");
- Serial.print(yaw);
- Serial.println("");
-}
-void readSignal() {
-  Serial.print(" Roll: ");
-  if (CH1_input - 1520 > 0) {
-    Serial.print(" x-Right ");
-  } else if (CH1_input - 1480 < 0){
-    Serial.print(" x-Left ");
-  } else {
-    Serial.print(" Stay ");
-  }
-  Serial.println(CH1_input);
-  Serial.print(" Pitch: ");
-  if (CH2_input - 1520 > 0) {
-    Serial.print("y-Right");
-  } else if (CH2_input - 1480 < 0){
-    Serial.print("y-Left");
-  } else {
-    Serial.print("Stay");
-  }
-  Serial.println(CH2_input);
-  Serial.print(" Y-translate: ");
-  if (CH3_input - 1520 > 0) {
-    Serial.print(" z-Up ");
-  } else if (CH3_input - 1480 < 0){
-    Serial.print(" z-Down ");
-  } else {
-    Serial.print(" Stay ");
-  }
-  Serial.println(CH3_input);
-  Serial.print(" Yaw: ");
-  if (CH4_input - 1520 > 0) {
-    Serial.print(" Rotate Clock ");
-  } else if (CH4_input - 1480 < 0){
-    Serial.print(" Rotate Anticlock ");
-  } else {
-    Serial.print(" Stay ");
-  }
-  Serial.println(CH4_input);
+ gyroZ = (Wire.read() << 8 | Wire.read()) / 131.0 - gyroZ_error;
 }
 
-void PID_Control() {
-  pid_time_prev = time;
-  time = millis();
-  pid_time_elp = (time - pid_time_prev)/1000;
-  
-  if (CH1_input < 1480) {
-    recX_angle = (1480 - CH1_input)/9.6; //Roll
-  } else {
-    recX_angle = (CH1_input - 1520)/9.6;
-  }
-  if (CH2_input < 1480) {
-    recY_angle = (1480 - CH2_input)/9.6; //Pitch
-  } else {
-    recY_angle = (CH2_input - 1520)/9.6;
-  }
-  if (CH4_input < 1480) {
-    recZ_angle = (1480 - CH4_input)/9.6; //Yaw
-  } else {
-    recZ_angle = (CH4_input - 1520)/9.6;
-  }
-  //Pitch
-  pitch_curr_error = (pitch - recY_angle);
-  pitch_p = pitch_curr_error * p_gain_pitch;
-  pitch_i += i_gain_pitch * pitch_curr_error;
-  if (pitch_i > max_pitch) {
-    pitch_i = max_pitch;
-  } else if (pitch_i < -1 * max_pitch) {
-    pitch_i = -1 * max_pitch;
-  };
-  pitch_d = d_gain_pitch * (pitch_curr_error - pitch_last_error)/(pid_time_elp);
-  pitch_output = pitch_p + pitch_i + pitch_d;
-  Serial.println("Pitch output:");
-  Serial.print("Error:");
-  Serial.print(pitch_curr_error - pitch_last_error);
-  Serial.print(pitch_p);
-  Serial.print("/");
-  Serial.print(pitch_i);
-  Serial.print("/");
-  Serial.print(pitch_d);
-  Serial.print("/");
-  Serial.print(pitch_output);
-  Serial.println("");
-  if (pitch_output > max_pitch) {
-    pitch_output = max_pitch;
-  } else if (pitch_output < -1 * max_pitch) {
-    pitch_output = -1 * max_pitch;
-  }
-  pitch_last_error = pitch_curr_error;
-  
-  //Roll
-  
-  roll_curr_error = (roll - recX_angle);
-  roll_p = roll_curr_error * p_gain_roll;
-  roll_i += i_gain_roll * roll_curr_error;
-  if (roll_i > max_roll) {
-    roll_i = max_roll;
-  } else if (pitch_i < -1 * max_roll) {
-    roll_i = -1 * max_roll;
-  };
-  roll_d = d_gain_roll * (roll_curr_error - roll_last_error)/(pid_time_elp);
-  roll_output = roll_p + roll_i + roll_d;
-  Serial.println("Roll output:");
-  Serial.print(roll_p);
-  Serial.print("/");
-  Serial.print(roll_i);
-  Serial.print("/");
-  Serial.print(roll_d);
-  Serial.print("/");
-  Serial.print(roll_output);
-  Serial.println("");
-  if (roll_output > max_roll) {
-    roll_output = max_roll;
-  } else if (roll_output < -1 * max_roll) {
-    roll_output = -1 * max_roll;
-  }
-  roll_last_error = roll_curr_error;
-  
-  //Yaw
-  
-  yaw_curr_error = (yaw - recZ_angle);
-  yaw_p = yaw_curr_error * p_gain_yaw;
-  yaw_i += yaw_curr_error * i_gain_yaw;
-  if (yaw_i > max_yaw) {
-    yaw_i = max_yaw;
-  } else if (yaw_i < -1 * max_yaw) {
-    yaw_i = -1 * max_yaw;
-  };
-  yaw_d = (yaw_curr_error - yaw_last_error)/(pid_time_elp);
-  yaw_output = yaw_p + yaw_i + d_gain_yaw * yaw_d;
-  if (yaw_output > max_yaw) {
-    yaw_output = max_yaw;
-  } else if (yaw_output < -1 * max_yaw) {
-    yaw_output = -1 * max_yaw;
-  }
-  yaw_last_error = yaw_curr_error;
-}
-
-void ESC_pulse() {
-  CH3_input = map(CH3_input,990,2000,850,1860);
-  if (CH3_input < 1000) {
-    esc1.writeMicroseconds(0);
-    esc2.writeMicroseconds(0);
-    esc3.writeMicroseconds(0);
-    esc4.writeMicroseconds(0);
-  } else {
-   esc1_pulse = CH3_input + roll_output + pitch_output + yaw_output;
-   esc2_pulse = CH3_input + roll_output - pitch_output - yaw_output;
-   esc3_pulse = CH3_input - roll_output - pitch_output + yaw_output;
-   esc4_pulse = CH3_input - roll_output + pitch_output - yaw_output;
-   if (esc1_pulse < 1000) {
-     esc1_pulse = 1000;
-   } else if (esc1_pulse < 1500) {
-    esc1_pulse = 1500;
-   }
-   if (esc2_pulse < 1000) {
-     esc2_pulse = 1000;
-   }else if (esc2_pulse > 1500) {
-    esc2_pulse = 1500;
-   }
-   if (esc3_pulse < 1000) {
-     esc3_pulse = 1000;
-   }else if (esc3_pulse > 1500) {
-    esc3_pulse = 1500;
-   }
-   if (esc4_pulse < 1000) {
-     esc4_pulse = 1000;
-   }else if (esc4_pulse > 1500) {
-    esc4_pulse = 1500;
-   }
-   Serial.print("ESC1: ");
-   Serial.println(esc1_pulse);
-   Serial.print("ESC2: ");
-   Serial.println(esc2_pulse);
-   Serial.print("ESC3: ");
-   Serial.println(esc3_pulse);
-   Serial.print("ESC4: ");
-   Serial.println(esc4_pulse);
-   esc1.writeMicroseconds(esc1_pulse);
-   esc2.writeMicroseconds(esc2_pulse);
-   esc3.writeMicroseconds(esc3_pulse);
-   esc4.writeMicroseconds(esc4_pulse); 
-  }
-}
-
+//Kalman Filter Functions
 void kalman_filter(float K_state,float K_u,float K_rate,float K_angle, float t_ellapsed) {
   K_state += t_ellapsed*K_rate;
   K_u += (t_ellapsed * t_ellapsed)*(4.0 *4.0);
@@ -379,4 +203,130 @@ void run_filters() {
   kalman_filter(K_pitch_angle,K_pitch_angle, gyroY, accY_angle,0.004);
   K_pitch_angle = K_output[0];
   K_pitch_angle_u = K_output[1];
+}
+
+void read_IMU() {
+  read_Acc();
+  read_Gyro();
+  run_filters();
+  roll = K_roll_angle;
+  pitch = K_pitch_angle;
+  yaw = yaw + gyroZ * time_ellp;
+}
+
+//Reading Transmitter Signal//
+void read_Signal() {
+  if (CH1_input < 1480) {
+    recX_angle = (1480 - CH1_input)/9.6; //Roll
+  } else {
+    recX_angle = (CH1_input - 1520)/9.6;
+  }
+  if (CH2_input < 1480) {
+    recY_angle = (1480 - CH2_input)/9.6; //Pitch
+  } else {
+    recY_angle = (CH2_input - 1520)/9.6;
+  }
+  if (CH4_input < 1480) {
+    recZ_angle = (1480 - CH4_input)/9.6; //Yaw
+  } else {
+    recZ_angle = (CH4_input - 1520)/9.6;
+  }
+}
+//PID Controller 
+
+void PID_Control() {
+  //Pitch
+  pitch_curr_error = (pitch - recY_angle);
+  pitch_p = pitch_curr_error * p_gain_pitch;
+  pitch_i += i_gain_pitch * pitch_curr_error;
+  if (pitch_i > max_pitch) {
+    pitch_i = max_pitch;
+  } else if (pitch_i < -1 * max_pitch) {
+    pitch_i = -1 * max_pitch;
+  };
+  pitch_d = d_gain_pitch * (pitch_curr_error - pitch_last_error);
+  pitch_output = pitch_p + pitch_i + pitch_d;
+  if (pitch_output > max_pitch) {
+    pitch_output = max_pitch;
+  } else if (pitch_output < -1 * max_pitch) {
+    pitch_output = -1 * max_pitch;
+  }
+  pitch_last_error = pitch_curr_error;
+  
+  //Roll
+  
+  roll_curr_error = (roll - recX_angle);
+  roll_p = roll_curr_error * p_gain_roll;
+  roll_i += i_gain_roll * roll_curr_error;
+  if (roll_i > max_roll) {
+    roll_i = max_roll;
+  } else if (pitch_i < -1 * max_roll) {
+    roll_i = -1 * max_roll;
+  };
+  roll_d = d_gain_roll * (roll_curr_error - roll_last_error);
+  roll_output = roll_p + roll_i + roll_d;
+  if (roll_output > max_roll) {
+    roll_output = max_roll;
+  } else if (roll_output < -1 * max_roll) {
+    roll_output = -1 * max_roll;
+  }
+  roll_last_error = roll_curr_error;
+  
+  //Yaw
+  
+  yaw_curr_error = (yaw - recZ_angle);
+  yaw_p = yaw_curr_error * p_gain_yaw;
+  yaw_i += yaw_curr_error * i_gain_yaw;
+  if (yaw_i > max_yaw) {
+    yaw_i = max_yaw;
+  } else if (yaw_i < -1 * max_yaw) {
+    yaw_i = -1 * max_yaw;
+  };
+  yaw_d = d_gain_yaw * (yaw_curr_error - yaw_last_error);
+  yaw_output = yaw_p + yaw_i + yaw_d;
+  if (yaw_output > max_yaw) {
+    yaw_output = max_yaw;
+  } else if (yaw_output < -1 * max_yaw) {
+    yaw_output = -1 * max_yaw;
+  }
+  yaw_last_error = yaw_curr_error;
+}
+
+//Providing Pulses to ESCs
+void ESC_Pulse() {
+  if (CH3_input < 1075) {
+    esc1.writeMicroseconds(1000);
+    esc2.writeMicroseconds(1000);
+    esc3.writeMicroseconds(1000);
+    esc4.writeMicroseconds(1000);
+  } else {
+   esc1_pulse = CH3_input + roll_output + pitch_output + yaw_output;
+   esc2_pulse = CH3_input + roll_output - pitch_output - yaw_output;
+   esc3_pulse = CH3_input - roll_output - pitch_output + yaw_output;
+   esc4_pulse = CH3_input - roll_output + pitch_output - yaw_output;
+   if (esc1_pulse < 1100) {
+     esc1_pulse = 1100;
+   } else if (esc1_pulse > 1600) {
+    esc1_pulse = 1600;
+   }
+   if (esc2_pulse < 1100) {
+     esc2_pulse = 1100;
+   }else if (esc2_pulse > 1600) {
+    esc2_pulse = 1600;
+   }
+   if (esc3_pulse < 1100) {
+     esc3_pulse = 1100;
+   }else if (esc3_pulse > 1600) {
+    esc3_pulse = 1600;
+   }
+   if (esc4_pulse < 1100) {
+     esc4_pulse = 1100;
+   }else if (esc4_pulse > 1600) {
+    esc4_pulse = 1600;
+   }
+   esc1.writeMicroseconds(esc1_pulse);
+   esc2.writeMicroseconds(esc2_pulse);
+   esc3.writeMicroseconds(esc3_pulse);
+   esc4.writeMicroseconds(esc4_pulse); 
+  }
 }
